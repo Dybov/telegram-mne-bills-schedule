fc109fddb2da6bd083f245ee14677808042d3089
Andrey Dybov <dybov.andrew@gmail.com>
2022-10-18 16:07:10 +0200

    Change credentials and set up Electricity file loading

--- bot/core.py
+++ bot/core.py
@@ -1,7 +1,9 @@
 import logging
+import io
 
 import asyncio
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types.input_file import InputFile
 
 from config import get_settings
 from i18n import _
@@ -27,8 +29,12 @@ async def send_welcome(message: types.Message):
 async def get_data(message: types.Message):
     await message.answer('Start request')
     logger.info("Start sending data")
-    msg = await get_message(message['chat']['id'])
+    msg, files = await get_message(message['chat']['id'])
     await message.answer(msg)
+    for file in files:
+        for filename, data in file.items():
+            file = InputFile(io.BytesIO(data), filename=filename)
+            await message.answer_document(file)
     logger.info("Data sent")
 
 
@@ -50,25 +56,32 @@ async def notify_admin(_: Dispatcher):
     if config.SEND_ON_START:
         await bot.send_message(
             config.admin_telegram_user_id, "Request bills...")
-        msg = await get_message(config.admin_telegram_user_id)
+        msg, files = await get_message(config.admin_telegram_user_id)
         await bot.send_message(config.admin_telegram_user_id, msg)
+        await bot.send_file()
     await bot.send_message(config.admin_telegram_user_id, "Bot started")
     logger.info("Notify sent")
 
 
 async def get_message(user_id):
     try:
-        messages = await asyncio.gather(
+        responses = await asyncio.gather(
             call_by_name('vodovod'),
             call_by_name('electricity'),
             call_by_name('komunalno'),
         )
+        messages = []
+        files = []
+        for res in responses:
+            messages.append(res['msg'])
+            if res.get('files'):
+                files.extend(res.get('files'))
         msg = '\n\n---\n\n'.join(messages)
-    except Exception as exc: # pylint: disable=broad-except
+    except Exception as exc:  # pylint: disable=broad-except
         msg = "Sorry something went wrong"
         if user_id == config.admin_telegram_user_id:
             msg = f"{msg}\n{repr(exc)}"
-    return msg
+    return msg, files
 
 
 def start(loop=None):
--- schedule/core.py
+++ schedule/core.py
@@ -1,14 +1,16 @@
 import calendar
 from datetime import datetime
 
+import requests
+
 from urllib.parse import urlencode
 from selenium import webdriver
-from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.firefox.options import Options
-
 
 
 async def call_by_name(name):
@@ -20,16 +22,18 @@ def get_credentials(name):
     return {
         'vodovod': {
             'func': get_vodovod_data,
-            'args': [64258, 'Racanovic Ljubo'],
+            # 'args': [64258, 'Racanovic Ljubo'],
+            'args': [64261, 'Racanovic Ljubo'],
         },
         'electricity': {
             'func': get_electicity_data,
-            'args': ['091683659', '15N4E5B2514808511'],
+            # 'args': ['091683659', '15N4E5B2514808511'],
+            'args': ['091683691', '15N4E5B2514808425'],
         },
         'komunalno': {
             'func': get_komunalno_data,
-            'args': ['G22791 Racanovic Ljubo',
-                     '85310 BUDVA, UL XVIII/SP.III/ST.16  AG 16'],
+            'args': ['G22794 Racanovic Ljubo',
+                     '85310 BUDVA, UL XVIII/SP.III/ST.19  AG 19'],
         }
     }.get(name)
 
@@ -37,7 +41,8 @@ def get_credentials(name):
 POST_PATH = 'https://vodovodbudva.me/zaduzenje/indexp.php'
 PDF_PATH = 'https://www.vodovodbudva.me/zaduzenje/search3.php'
 ELECT = 'https://www.racun.epcg.com/provjera-racuna'
-
+ELECT2 = 'https://www.racun.epcg.com/pdf-bill/1'
+# class=pdf-download
 
 async def get_vodovod_data(sifra, name, url=POST_PATH, pdf_link=PDF_PATH):
     params = urlencode({
@@ -59,7 +64,7 @@ async def get_vodovod_data(sifra, name, url=POST_PATH, pdf_link=PDF_PATH):
         [f"{key}: {value}" for key, value in results.items()])
 
     msg = f'Vodovod:\n{results_msg}\n\n{full_url}\n\nPDF:{pdf_url}'
-    return msg
+    return {'msg': msg}
 
 
 async def get_electicity_data(home, brojila, url=ELECT):
@@ -67,21 +72,28 @@ async def get_electicity_data(home, brojila, url=ELECT):
               'edit-home-broj-brojila': brojila}
     # full_url = f'{url}?{urlencode(params)}'
     full_url = url
-    
+
     results = {
         'Pretplatni/Naplatni broj *': home,
         'Broj brojila': brojila,
     }
-    
+
     with Scrapper(full_url, method='POST', data=params, wait='total') as scr:
         results['Ukupno za uplatu'] = scr.get_by_class('total')
         results['Poslednji račun'] = scr.get_by_class('last-bill')
+        session = requests.session()
+        selenium_cookies = scr.driver.get_cookies()
+        for cookie in selenium_cookies:
+            session.cookies.set(cookie["name"], cookie["value"])
+
+        pdf_response = session.get(ELECT2)
+        pdf_bytes = pdf_response.content
 
     results_msg = "\n".join(
         [f"{key}: {value}" for key, value in results.items()])
 
     msg = f'Electricity:\n{results_msg}\n\n{full_url}'
-    return msg
+    return {'msg': msg, 'files': [{'Electricity.pdf': pdf_bytes}]}
 
 
 class KomunalnoPrice:
@@ -98,9 +110,13 @@ class KomunalnoPrice:
 
     @staticmethod
     def get_value(key):
-        if key in [6, 7, 8]:
-            return 4.32
-        return 3.33
+        # if key in [6, 7, 8]:
+        if key in [6, 7, 8, 9]:
+            return 5.5
+            # return 4.32
+        # return 3.33
+        return 4.23
+
 
 async def get_komunalno_data(korisnic, adress):
     now = datetime.now()
@@ -117,13 +133,14 @@ async def get_komunalno_data(korisnic, adress):
         [f"{key}: {value}" for key, value in results.items()])
 
     msg = f'Komunalno:\n{results_msg}'
-    return msg
+    return {'msg': msg}
 
 
 class Scrapper:
     def __init__(self, url, method="GET", data=None, wait='TotalAmountNE'):
         opt = get_options()
         self.driver = webdriver.Firefox(options=opt)
+        self.driver.set_window_size(200, 200)
         meth = method.upper()
         self.data = data
         self.driver.get(url)
@@ -148,6 +165,11 @@ class Scrapper:
                 WebDriverWait(self.driver, 5).until(
                     EC.presence_of_element_located((By.CLASS_NAME, wait)))
 
+    def open_page(self, url, until):
+        # self.driver.set_timeoi
+        self.driver.get(url)
+        WebDriverWait(self.driver, 1).until(until)
+
     def __enter__(self):
         return self
 
@@ -162,8 +184,27 @@ class Scrapper:
             find_element_by_class_name('value').get_attribute('innerHTML')
 
 
+def pdf_page_loaded(driver):
+    try:
+        element = driver.find_element(
+            By.CSS_SELECTOR, "*[data-page-number='2'][data-loaded='true']")
+    except NoSuchElementException:
+        return False
+    res = element.get_attribute("data-loaded") == "true"
+    return res
+
+
 def get_options():
     options = Options()
     options.add_argument("--headless")
-    options.add_argument("--window-size=800,600")
+    # options.add_argument("--window-size=800,600")
+
+
+    options.set_preference("browser.download.folderList", 2)
+    options.set_preference("browser.download.dir", "/tmp/")
+    options.set_preference("browser.download.useDownloadDir", True)
+    options.set_preference("browser.download.alwaysOpenPanel", False)
+    options.set_preference("browser.download.viewableInternally.enabledTypes", "")
+    options.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/pdftext/plainapplication/texttext/xmlapplication/xml")
+    # options.set_preference("pdfjs.disabled", True)  # disable the built-in PDF viewer
     return options
